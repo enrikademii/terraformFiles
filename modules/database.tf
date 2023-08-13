e68fdfc77a0cab9a97081e9b07754a879c793562
enrikademii <135328149+enrikademii@users.noreply.github.com>
2023-08-13 16:06:52 +0200

    Update database.tf

--- modules/database.tf
+++ modules/database.tf
@@ -1,1 +1,11 @@
-rds
+resource "aws_db_instance" "project_db" {
+  allocated_storage    = 5
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  db_name              = "project_db"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
